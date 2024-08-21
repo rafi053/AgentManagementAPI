28c59b5f8e4817ce414dfc7db00c726a41849c6c
rafi053 <Rafi053313@gmail.com>
2024-08-21 11:01:21 +0300

    Add project files.

---
+++ AgentManagementAPI/Enums/Status.cs
@@ -0,0 +1,6 @@
+﻿namespace AgentManagementAPI.Enums
+{
+    public enum Status
+    {
+    }
+}
---
+++ AgentManagementAPI/Models/Agent.cs
@@ -0,0 +1,22 @@
+﻿
+using AgentManagementAPI.Enums;
+
+namespace AgentManagementAPI.Models
+{
+    public class Agent
+    {
+        public int Id { get; set; }
+
+        public string? Image { get; set; }
+
+        public string Nickname { get; set; }
+
+        public Location location { get; set; }
+
+        public Status status { get; set; }
+
+
+
+
+    }
+}
