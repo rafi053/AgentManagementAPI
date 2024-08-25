2c1c8ff02f6f2715fea62027af8e32dd194c6341
rafi053 <Rafi053313@gmail.com>
2024-08-25 13:32:02 +0300

    טיפול בטוקן

---
+++ AgentManagementAPI/Controllers/LoginController.cs
@@ -0,0 +1,72 @@
+﻿using AgentManagementAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+
+
+using System.Text;
+
+namespace AgentManagementAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private string GenerateToken(string userIP)
+        {
+            // token handler can create token
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            string secretKey = "1234dyi5fjthgjdndfadsfgdsjfgj464twiyyd5ntyhgkdrue74hsf5ytsusefh55678"; //TODO: remove this from code
+            byte[] key = Encoding.ASCII.GetBytes(secretKey);
+
+            // token descriptor describe HOW to create the token
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                // things to include in the token
+                Subject = new ClaimsIdentity(
+                    new Claim[]
+                    {
+                    new Claim(ClaimTypes.Name, userIP),
+                    }
+                ),
+                // expiration time of the token
+                Expires = DateTime.UtcNow.AddSeconds(30),
+                // the secret key of the token
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature
+                    )
+            };
+
+            // creating the token
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            // converting the token to string
+            var tokenString = tokenHandler.WriteToken(token);
+            return tokenString;
+        }
+
+        //[HttpPost]
+        //public IActionResult Login(LoginObject loginObject)
+        //{
+        //    if (loginObject.UserName == "admin" &&
+        //        loginObject.Password == "123456")
+        //    {
+
+        //        // getting the user (requester) IP
+        //        string userIP = HttpContext.Connection.RemoteIpAddress.MapToIPv4().ToString();
+
+        //        return StatusCode(200
+        //            , new { token = GenerateToken(userIP) }
+        //            );
+        //    }
+        //    return StatusCode(StatusCodes.Status401Unauthorized,
+        //            new { error = "invalid credentials" });
+
+
+        //}
+    }
+
+}
---
+++ AgentManagementAPI/Middlewares/Global/GlobalLoggingMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.Text.Json;
+
+
+namespace AgentManagementAPI.Middlewares.Global
+{
+    public class GlobalLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalLoggingMiddleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var request = context.Request;
+            Console.WriteLine($"Got Request to server: {request.Method} {request.Path}\n" +
+                              $"From IP: {request.HttpContext.Connection.RemoteIpAddress}");
+            await this._next(context);
+        }
+    }
+}
